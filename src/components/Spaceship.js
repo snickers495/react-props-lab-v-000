afdc4262e1bf690ad60b5bb8a9cb045d965a21ef
Lisa Lee <snickers495@gmail.com>
2018-02-05 16:06:17 +0000

    automatically backed up by learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Spaceship extends React.Component {
-  defaultProps
   render(){
     return (
       <title>{this.props.name}</title>
