d58a0febfe3b6656810d56c59e918d7a186618ba
Lisa Lee <snickers495@gmail.com>
2018-02-05 16:04:47 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import Spaceship from './components/Spaceship';
 ReactDOM.render(
   <Spaceship name="Millennium Falcon",
   speed="0",
-  hasRockets />,
+  hasRockets="" />,
   document.getElementById('global')
 );
